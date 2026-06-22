296797eb999ddd327e1c0f926c3dcd49b6c9006b
Md Zeeshan Mushtaque <zeeshanmushtaque786@gmail.com>
2026-06-22 16:54:12 +0530

    Remove README.md and test.java; add solution.java and test.cpp with initial implementations

---
+++ solution.java
@@ -0,0 +1,41 @@
+import java.util.*;
+public class solution {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        // int marks = sc.nextInt();
+        // int leaves = sc.nextInt();
+        // if(marks>50 && leaves<10){
+        //     System.out.println("Pass");
+        // }else{
+        //     System.out.println("Fail");
+        // }
+
+        //SWITCH
+        // char grade='A';
+        // switch(grade){
+        //     case 'A':
+        //         System.out.println("Excellent");
+        //         break;
+        //     case 'B':
+        //         System.out.println("Good");
+        //         break;
+        //     default:
+        //         System.out.println("Invalid");
+        // }
+
+        // Ternary operator
+        // int x= sc.nextInt();
+        // int y= sc.nextInt();
+        // System.out.println((x > y) ? x : y);
+
+        // LOOPS
+
+        int n=5;
+        for(int i=1; i<=n;i++){
+            for(int j=1; j<=n; j++){
+            System.out.print("* ");
+        }
+        System.out.println();
+    }
+    }
+}
--- test.java
+++
@@ -1,5 +0,0 @@
-package SummerPEPLPU;
-
-public class test {
-    
-}
